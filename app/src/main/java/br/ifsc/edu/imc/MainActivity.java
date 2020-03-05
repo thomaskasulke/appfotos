6187558598e2828e54b63e0c168dd368355ef785
Axel <axelgeorg16@gmail.com>
2020-03-05 11:29:18 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/imc/MainActivity.java
@@ -0,0 +1,23 @@
+package br.ifsc.edu.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+    public void mudatexto (View view) {
+        imageView.setImageResource(R.drawable.cachorro);
+    }
+
+
+}

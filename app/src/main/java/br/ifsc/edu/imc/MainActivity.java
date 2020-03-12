562946c6317875087f4b3c8df9d1039f3ab6b9ba
Axel <axelgeorg16@gmail.com>
2020-03-12 11:15:20 -0300

    cachorro

--- app/src/main/java/br/ifsc/edu/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/imc/MainActivity.java
@@ -6,18 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    List<Integer> lista_imagens;
+    int posicaoImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
-    }
-    public void mudatexto (View view) {
-        imageView.setImageResource(R.drawable.cachorro);
+        lista_imagens = Arrays.asList(
+                R.drawable.cachorro,
+                R.drawable.gardem,
+                R.drawable.happy,
+                R.drawable.patinho);
+        posicaoImage = 0;
     }
 
+    public void mudafoto(View view) {
+        if (posicaoImage < lista_imagens.size() - 1) {
+            imageView.setImageResource(lista_imagens.get(posicaoImage));
+        } else {
+            posicaoImage = 0;
+            imageView.setImageResource(lista_imagens.get(posicaoImage));
+        }
+        posicaoImage++;
 
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
+
